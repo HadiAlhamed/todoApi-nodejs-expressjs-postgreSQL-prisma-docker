9f1f35a193197df661af1a1eb46c19d4d5792298
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-22 00:21:19 +0300

    user prisma orm to handle register and login logic

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,7 +1,4 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import db from '../db.js';
 import { register, login } from '../controllers/auth-controller.js';
 const router = express.Router();
 
